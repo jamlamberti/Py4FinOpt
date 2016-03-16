600cba1feed02ba2ac30260bce42c0f7cc0cf7a9
Jack Lamberti <jal416@lehigh.edu>
2016-03-15 21:03:39 -0400

    Cleaned up var names

--- optimization/mad.py
+++ optimization/mad.py
@@ -9,34 +9,34 @@ def mad(returns, m, short_sales=False):
     means = np.mean(returns, axis=1)
     N, T = returns.shape
     
-    CMAD = np.zeros((T+N, 1))
-    CMAD[0:T, 0] = 1.0/T
-    CMAD[T:, 0] = 0.0
+    c = np.zeros((T+N, 1))
+    c[0:T, 0] = 1.0/T
+    c[T:, 0] = 0.0
     
-    beqMAD = [1.0]
+    beq = [1.0]
 
-    AMAD1 = -np.concatenate((np.eye(T), np.transpose(-returns+(means*np.ones((1, T))))), axis=1)
-    AMAD2 = -np.concatenate((np.eye(T), np.transpose(returns-(means*np.ones((1, T))))), axis=1)
-    AMAD3 = np.zeros((1, T+N))
-    AMAD3[0, :] = [0.0 for _ in range(T)] + list(-means)
-    AMAD = np.concatenate((AMAD1, AMAD2, AMAD3), axis=0)
+    A1 = -np.concatenate((np.eye(T), np.transpose(-returns+(means*np.ones((1, T))))), axis=1)
+    A2 = -np.concatenate((np.eye(T), np.transpose(returns-(means*np.ones((1, T))))), axis=1)
+    A3 = np.zeros((1, T+N))
+    A3[0, :] = [0.0 for _ in range(T)] + list(-means)
+    A = np.concatenate((A1, A2, A3), axis=0)
 
-    bMAD = np.zeros((2*T+1, 1))
-    bMAD[2*T, 0] = -m
+    b = np.zeros((2*T+1, 1))
+    b[2*T, 0] = -m
 
     if not short_sales:
-        AMAD = np.concatenate((AMAD, np.concatenate((np.zeros((N, T)), -np.eye(N)), axis=1)), axis=0)
-        bMAD = np.concatenate((bMAD, np.zeros((N, 1))), axis=0)
+        A = np.concatenate((A, np.concatenate((np.zeros((N, T)), -np.eye(N)), axis=1)), axis=0)
+        b = np.concatenate((b, np.zeros((N, 1))), axis=0)
     
-    AeqMAD = np.zeros((1, T+N))
-    AeqMAD[0, :] = [0.0 for _ in range(T)] + [1.0 for _ in range(N)]
+    Aeq = np.zeros((1, T+N))
+    Aeq[0, :] = [0.0 for _ in range(T)] + [1.0 for _ in range(N)]
     
     sol = solvers.lp(
-        matrix(CMAD),
-        matrix(AMAD),
-        matrix(bMAD),
-        matrix(AeqMAD),
-        matrix(beqMAD)
+        matrix(c),
+        matrix(A),
+        matrix(b),
+        matrix(Aeq),
+        matrix(beq)
     )
 
     return sol['x'][T:]
