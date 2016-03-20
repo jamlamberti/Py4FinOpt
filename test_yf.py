947a4e87f9f0f6fe39789c00b002f1b862db708f
Jack Lamberti <jal416@lehigh.edu>
2016-03-20 01:10:31 -0400

    Some minor changes to the smoke test

--- test_yf.py
+++ test_yf.py
@@ -1,3 +1,8 @@
+"""A is-it-up test for YF"""
 import yahoo_finance as yf
-s = yf.Share('AAPL')
-assert len(s.get_historical('2015-01-01', '2016-01-01')) > 0
+
+if __name__ == '__main__':
+    # not going to make share a constant
+    # pylint: disable=invalid-name
+    share = yf.Share('AAPL')
+    assert len(share.get_historical('2015-01-01', '2016-01-01')) > 0
