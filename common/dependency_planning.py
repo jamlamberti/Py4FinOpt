d53f8795746f957ab7634d7cd4b307023dc071de
Jack Lamberti <jal416@lehigh.edu>
2016-03-21 15:56:53 -0400

    Updated dep planner, removed duplicate function

--- common/dependency_planning.py
+++ common/dependency_planning.py
@@ -1,96 +1,98 @@
+"""Check for any dependencies between a set of tasks or functions"""
+from __future__ import print_function
 import networkx as nx
-import numpy as np
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 class DependencyResolver(object):
+    """Determines the optimal order to run a set of tasks"""
     def __init__(self):
         self.dependencies = {}
-    
+        self.sol = None
+
     def add_task(self, task):
+        """Add a taski (e.g node)"""
         if task not in self.dependencies:
             self.dependencies[task] = []
-    
+
     def add_dep(self, task, dep):
+        """
+        Add a dependency (e.g. edge)
+            - if the task or dep hasn't been added, they will also be added
+        """
+
         self.add_task(dep)
         self.add_task(task)
         self.dependencies[task].append(dep)
-    
+
     def visualize(self):
-        G = nx.DiGraph()
+        """Visualize the graph"""
+        graph = nx.DiGraph()
         for task in self.dependencies.keys():
             for dep in self.dependencies[task]:
-                G.add_edges_from([(dep, task)])
-        pos = nx.spring_layout(G)
-        nx.draw_networkx_nodes(G, pos)
-        nx.draw_networkx_edges(G, pos, arrows=True, width=2)
-        #nx.draw_networkx_edge_labels(G, pos)
-        nx.draw(G, pos, node_size=3000)
+                graph.add_edges_from([(dep, task)])
+        pos = nx.spring_layout(graph)
+        nx.draw_networkx_nodes(graph, pos)
+        nx.draw_networkx_edges(graph, pos, arrows=True, width=2)
+        #nx.draw_networkx_edge_labels(graph, pos)
+        nx.draw(graph, pos, node_size=3000)
         plt.savefig('out.png')
 
     def _dep_resolve(self, node, resolved, unresolved):
+        """Solve the problem"""
         unresolved.append(node)
         for edge in self.dependencies[node]:
             if edge not in resolved:
                 if edge in unresolved:
-                    raise Exception('Circular reference detected: %s -&gt; %s' % (node.name, edge.name))
+                    raise Exception(
+                        'Circular reference detected: %s -&gt; %s'
+                        % (node.name, edge.name))
                 self._dep_resolve(edge, resolved, unresolved)
         resolved.append(node)
         unresolved.remove(node)
         return resolved
-        
+
     def generate_solution(self, goal):
+        """Generates the solution for running goal"""
         self.sol = self._dep_resolve(goal, [], [])
-        
+
     def validate_solution(self, path, completed):
-        print "Analyzing solution:"
-        print path
-        print completed
+        """Validates that we found a feasible and optimal solution"""
+        print("Analyzing solution:")
+        print(path)
+        print(completed)
         while len(path) > 0:
             cur = path[0]
             if cur in completed:
-                print "Solution suboptimal... repeating task", cur
+                print("Solution suboptimal... repeating task", cur)
             del path[0]
             for dep in self.dependencies[cur]:
                 if dep == cur:
                     # Circular error
                     return False
                 elif dep not in completed:
-                    print "Cannot do", cur, "until", dep
+                    print("Cannot do", cur, "until", dep)
                     return False
             completed.append(cur)
         return True
 
-    def visualize_solution(sol=None):
-        if sol is None:
-            sol = self.sol
-        G = nx.DiGraph()
-        for task in self.dependencies.keys():
-            for dep in self.dependencies[task]:
-                G.add_edges_from([(dep, task)])
-        pos = nx.spring_layout(G)
-        nx.draw_networkx_nodes(G, pos)
-        nx.draw_networkx_edges(G, pos, arrows=True, width=2)
-        #nx.draw_networkx_edge_labels(G, pos)
-        nx.draw(G, pos, node_size=3000)
-        plt.savefig('out.png')
-
 def test_case():
-    dr = DependencyResolver()
-    dr.add_task('arithmetic mean')
-    dr.add_task('geometric mean')
-    dr.add_task('median')
-    dr.add_task('quartiles')
-    dr.add_task('standard deviation')
-    dr.add_dep('downside standard deviation', 'arithmetic mean')
-    dr.add_dep('MAD', 'arithmetic mean')
-    dr.add_dep('sharpe ratio', 'standard deviation')
-    dr.add_dep('sortino ratio', 'downside standard deviation')
-    dr.visualize()
-    dr.generate_solution('MAD')
-    print dr.sol
-    dr.validate_solution(dr.sol, [])
+    """A simple smoke test"""
+    depr = DependencyResolver()
+    depr.add_task('arithmetic mean')
+    depr.add_task('geometric mean')
+    depr.add_task('median')
+    depr.add_task('quartiles')
+    depr.add_task('standard deviation')
+    depr.add_dep('downside standard deviation', 'arithmetic mean')
+    depr.add_dep('MAD', 'arithmetic mean')
+    depr.add_dep('sharpe ratio', 'standard deviation')
+    depr.add_dep('sortino ratio', 'downside standard deviation')
+    depr.visualize()
+    depr.generate_solution('MAD')
+    print(depr.sol)
+    depr.validate_solution(depr.sol, [])
 
 
 if __name__ == "__main__":
