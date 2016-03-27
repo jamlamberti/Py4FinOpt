79dac2efca2b97c461dbe1599881ed340054f8c4
Jack Lamberti <jal416@lehigh.edu>
2016-03-27 14:32:25 -0400

    Bumped cov on metrics, test if previous results are not leveraged

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -5,7 +5,8 @@ from metrics import metrics
 
 def test_metrics():
     """A simple smoke test"""
-    res = metrics.describe(1. * np.array([range(1, 10)]))
+    data = 1. * np.array([range(1, 10)])
+    res = metrics.describe(data)
     assert res['arithmetic_mean'] == 5.0
     assert abs(res['downside_variance']-3.33333) < 0.00001
     assert all([
@@ -15,3 +16,13 @@ def test_metrics():
     assert abs(res['standard_deviation'] - 2.7386) < 0.0001
     assert abs(res['mean_absolute_deviation']-2.22222) < 0.00001
     assert abs(res['geometric_mean']-4.147166) < 0.00001
+
+    funcs = [
+        metrics.arithmetic_mean, metrics.geometric_mean, metrics.median,
+        metrics.standard_deviation, metrics.quartiles,
+        metrics.interquartile_range, metrics.downside_variance,
+        metrics.mean_absolute_deviation
+    ]
+
+    for func in funcs:
+        assert func(data) == res[func.__name__]
