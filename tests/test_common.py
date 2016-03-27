b03c09d3f4db96aafbf283103a1369b091fff0ba
Jack Lamberti <jal416@lehigh.edu>
2016-03-26 21:55:11 -0400

    Added explict testing for config.py

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,12 +1,11 @@
 """A set of test cases for common"""
 from __future__ import print_function
 import os
-from common import credential_manager, dependency_planning
 
 
 def test_sqlite_connection():
     """A simple smoke test"""
-    from common import sqlite_manager
+    from common import credential_manager, sqlite_manager
     cred_mgr = credential_manager.CredentialManager(
         host=None,
         user=None,
@@ -22,8 +21,9 @@ def test_sqlite_connection():
     os.remove('test.db')
 
 
-def test_case():
+def test_dependency_planning():
     """A simple smoke test"""
+    from common import dependency_planning
     depr = dependency_planning.DependencyResolver()
     depr.add_task('arithmetic mean')
     depr.add_task('geometric mean')
@@ -40,3 +40,32 @@ def test_case():
     depr.generate_solution('MAD')
     print(depr.sol)
     assert depr.validate_solution(depr.sol, [])
+
+
+def test_config_section():
+    """Run tests over the testing section of the config file"""
+    from common import config
+    test_config = config.Section('testing')
+    str_test = test_config.get('s')
+    assert str_test == 'abc'
+    assert isinstance(str_test, str) or isinstance(str_test, unicode)
+
+    int_test = test_config.getint('x')
+    assert isinstance(int_test, int)
+    assert int_test == 1
+
+    float_test = test_config.getfloat('f')
+    assert isinstance(float_test, float)
+    assert float_test == -0.05
+
+    true_test = test_config.getboolean('bt')
+    assert true_test
+    assert isinstance(true_test, bool)
+
+    false_test = test_config.getboolean('bf')
+    assert not false_test
+    assert isinstance(false_test, bool)
+
+    list_test = test_config.getlist('li')
+    assert len(list_test) == 3
+    assert isinstance(list_test, list)
