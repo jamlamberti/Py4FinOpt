eea22594f4742991003b9e56af91d4ab9a203378
Jack Lamberti <jal416@lehigh.edu>
2016-04-01 05:19:28 -0400

    Test code and jump_diffusion model

--- backtest/sqrt_diffusion.py
+++ backtest/sqrt_diffusion.py
@@ -1,12 +1,13 @@
 """square-root diffusion"""
 import numpy as np
 
+
 def sqrt_diffusion(mu, sigma, start, steps, mean_reversion=2.0, t_step=1.0):
     """Sqrt diffusion implementation"""
     prev = start
     prices = [start]
-    for _ in range(steps-1):
-        prev = (prev + mean_reversion*(mu - prices[-1])*t_step
-                + sigma*np.sqrt(prices[-1]*t_step)*np.random.randn())
+    for _ in range(steps - 1):
+        prev = (prev + mean_reversion * (mu - prices[-1]) * t_step +
+                sigma * np.sqrt(prices[-1] * t_step) * np.random.randn())
         prices.append(max(0.0, prev))
     return prices
--- tests/test_backtest.py
+++ tests/test_backtest.py
@@ -1,4 +1,6 @@
 """A collection of tests for backtest module"""
+
+
 def test_gbm_model():
     """Test GBM Model"""
     from backtest.gbm_model import gbm_model
@@ -6,6 +8,8 @@ def test_gbm_model():
     sim = gbm_model(100.0, 1.0, 105.0, steps)
     # Probably want to check mean and sigma
     assert len(sim) == steps
+    assert all([i >= 0.0 for i in sim])
+
 
 def test_sqrt_diffusion():
     """Test Square-Root Diffusion Model"""
@@ -13,3 +17,13 @@ def test_sqrt_diffusion():
     steps = 100
     sim = sqrt_diffusion(100.0, 1.0, 80.0, steps, mean_reversion=3.0)
     assert len(sim) == steps
+    assert all([i >= 0.0 for i in sim])
+
+
+def test_jump_diffusion():
+    """Test Merton Jump Diffusion"""
+    from backtest.jump_diffusion import jump_diffusion
+    steps = 100
+    sim = jump_diffusion(100.0, 1.0, 105.0, steps)
+    assert len(sim) == steps
+    assert all([i >= 0.0 for i in sim])
