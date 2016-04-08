9c27f294e99648aca113a40449407e55a667f270
Jack Lamberti <jal416@lehigh.edu>
2016-04-08 18:54:57 -0400

    Continuation indent fix

--- backtest/jump_diffusion.py
+++ backtest/jump_diffusion.py
@@ -1,5 +1,6 @@
 """Jump Diffusion Model"""
 import numpy as np
+import numpy.random as npr
 
 
 def jump_diffusion(
@@ -8,20 +9,21 @@ def jump_diffusion(
         start,
         steps,
         t_step=1.0,
-        riskless_short_rate=0.05,
+        riskless_sr=0.05,
         delta=0.25,
         poisson_intensity=0.75):
     """Merton Jump Diffusion implementation"""
     jump_correction = poisson_intensity * (np.exp(mu + 0.5 * delta**2) - 1)
     prices = [start]
-
+    # Pylint and pep8 disagree on continuation indents here
+    # I am choosing pep8 over pylint here
+    # pylint: disable=bad-continuation
     for _ in range(steps - 1):
         prices.append(max(
             0.0,
             prices[-1] * (np.exp(
-                (riskless_short_rate - jump_correction - 0.5 * sigma**2) *
-                t_step +
-                sigma * np.sqrt(t_step) * np.random.standard_normal()) +
-                (np.exp(mu + delta * np.random.standard_normal()) - 1) *
-                np.random.poisson(poisson_intensity * t_step))))
+                t_step * (riskless_sr - jump_correction - 0.5 * sigma**2) +
+                sigma * np.sqrt(t_step) * npr.standard_normal()) +
+                (np.exp(mu + delta * npr.standard_normal()) - 1) *
+                npr.poisson(poisson_intensity * t_step))))
     return prices
